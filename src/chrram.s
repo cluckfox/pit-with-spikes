dbb2abf0a23a9907c4ed0f13d95a3da2be005c0f
cluckfox <77713230+cluckfox@users.noreply.github.com>
2021-11-27 19:55:37 -0500

    Wrote a video copy routine to load palette data
    Used video copy routine to load pattern data

--- src/chrram.s
+++ src/chrram.s
@@ -7,6 +7,7 @@
 ; the copyright notice and this notice are preserved in all source
 ; code copies.  This file is offered as-is, without any warranty.
 ;
+.include "ldvram.inc"
 .include "nes.inc"
 .include "mmc1.inc"
 .export load_chr_ram_far
@@ -19,27 +20,8 @@ template_chr:
 ;;
 ; Loads 8192 bytes of uncompressed data into CHR RAM.
 .proc load_chr_ram_far
-srclo = 0
-srchi = 1
-
-  lda #<template_chr
-  sta srclo
-  lda #>template_chr
-  sta srchi
-  ldy #0
-  sty PPUADDR  ; set starting location in CHR RAM to $0000
-  sty PPUADDR
-  ldx #>8192  ; X counts the remaining length of CHR in 256-byte units
-loop:
-  lda (srclo),y
-  sta PPUDATA
-  iny
-  bne loop
-  ; after every 256th byte we end up here
-  inc srchi  ; move on to the next set of 256 bytes of CHR
-  dex
-  bne loop
-  jmp bankrts
+	LDVRAM	template_chr, 8192, $0000
+	jmp	bankrts
 .endproc
 
 
